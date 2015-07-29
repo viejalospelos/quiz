83d52af50a155d3dcf8fdf2f0fb09d2c5442c94b
viejalospelos <viejalospelos@gmail.com>
2015-07-29 12:48:40 +0200

    creación de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -37,6 +37,26 @@ exports.answer=function(req,res){
 	
 };
 
+
+// GET /quizes/new
+exports.new = function(req, res) {
+  var quiz = models.Quiz.build(
+    {pregunta: "Pregunta", respuesta: "Respuesta"}
+  );
+
+  res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build( req.body.quiz );
+
+// guarda en DB los campos pregunta y respuesta de quiz
+  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+    res.redirect('/quizes');  
+  })   // res.redirect: Redirección HTTP a lista de preguntas
+};
+
 //GET quizes/author
 exports.author=function(req,res){
 	res.render('author');
