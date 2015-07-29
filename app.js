7026432919aa2372494aad939a0fd6ebd9637fd1
viejalospelos <viejalospelos@gmail.com>
2015-07-29 20:47:35 +0200

    editar preguntas

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 
 var partials=require('express-partials');
 var routes = require('./routes/index');
-
+var methodOverride = require('method-override');
 
 var app = express();
 
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(methodOverride('_method'));
 app.use('/', routes);
 
 
